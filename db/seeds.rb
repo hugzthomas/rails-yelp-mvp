359ec8ad3e9ff0ddf557c938ae95d83918f9e30e
hugz thomas <hugues.lam.thomas@gmail.com>
2017-02-09 11:07:11 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create!(name: "Mc Donalds", address: "46 rue de la boustipaille", phone_number: "0677951510", category: "Fast Food")
+Restaurant.create!(name: "Le tir Bouchon", address: "47 rue tiquetonne", phone_number: "0677951510", category: "Sud-Ouest" )
+Restaurant.create!(name: "Pere et fils", address: "66 rue du cherche-midi", phone_number: "0677951510", category: "French")
+Restaurant.create!(name: "L'épicure", address: "13 impasse du chevalier de la bar", phone_number: "0677951510", category: "Gastronomy" )
+Restaurant.create!(name: "L'ami Louis", address: "113 rue du bled", phone_number: "0677951510", category: "Bistronomy")
